f05f7d4679459fdadb21bead9317d83b15e064f5
najwa-nh <najwa.nh29@gmail.com>
2024-10-30 11:44:58 -0400

    course schedule view

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -55,6 +55,11 @@ exports.getJoin = (req, res) => {
     res.render('./officeHours/join'); // Render the create.ejs view
 };
 
+//get the course schedule view - Najwa
+/* exports.schedule = (req, res) => {
+    res.render('./officeHours/schedule');
+}*/
+
 
 //TODO: test once route is created
 exports.join = (req, res, next) => {
